10433430671328a6500083ceb072c66f9f6ae29f
JULIANOCAMPOS\julia <julianovincedecampos@hotmail.com>
2026-03-02 13:05:42 -0300

    chore(core): add package __init__ and __main__ entrypoints

---
+++ src/iam_governance/__init__.py
@@ -0,0 +1,4 @@
+"""IAM Governance Lab - Mini IGA simulator."""
+
+__version__ = "0.1.0"
+__author__ = "IAM Governance Lab Contributors"
---
+++ src/iam_governance/__main__.py
@@ -0,0 +1,5 @@
+"""Allow running as: python -m iam_governance"""
+from iam_governance.cli import app
+
+if __name__ == "__main__":
+    app()
